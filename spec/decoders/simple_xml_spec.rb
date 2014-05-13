bfc7f5c6651ccc64fdf57732f8d2554b4de19667
Ben Dunkley <ben@chemica.co.uk>
2014-05-13 16:00:33 +0100

    Simple test for XML decoding

---
+++ spec/decoders/simple_xml_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+require 'velvet_cushion/decoders/simple_xml'
+
+describe VelvetCushion::Decoders::SimpleXml do
+  it "turns simple XML into an object" do
+    xml = '<aa><bb>1</bb><cc>2</cc><dd>word</dd></aa>'
+    object = VelvetCushion::Decoders::SimpleXml.new.decode(xml)
+    expect(object).to eq({'aa' => {'bb' => '1', 'cc' => '2', 'dd' => 'word'}})
+  end
+end
