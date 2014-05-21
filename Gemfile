c07f2df56a3822ed52a40199f584fc860bc24f49
Ben Dunkley <ben@chemica.co.uk>
2014-05-22 00:54:16 +0100

    Stuff model from JSON non-recursively

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gemspec
 
 gem 'rake'
 gem 'debugger', :platforms => [:ruby_18, :ruby_19]
-gem 'byebug', :platforms => [:ruby_20]
+gem 'byebug', :platforms => [:ruby_20, :ruby_21]
 
 gem 'jdbc-sqlite3', :platforms => [:jruby]
 gem 'activerecord-jdbcsqlite3-adapter', :platforms => [:jruby]
--- lib/velvet_cushion/active_record/acts/cushion.rb
+++ lib/velvet_cushion/active_record/acts/cushion.rb
@@ -5,55 +5,39 @@ module ActiveRecord
         base.extend(ClassMethods)
       end
 
+      # Methods available to "acts_as_cushion" models.
+      module InstanceMethods
+
+        def stuff_with(source, options = {})
+          stuffer = VelvetCushion::Stuffer.new
+          stuffer.stuff self, source, options
+        end
+
+      end
+
       # An +acts_as+ module that adds the ability to stuff model instances full of data (normally from a JSON API).
       # Nested objects will be guessed from model associations.
       module ClassMethods
         # config options are:
         #
-        # * +decoder+ - specifies the decoder to use for stuffing this model
+        # * +decoder+ - specifies the default decoder to use for stuffing this model
         def acts_as_cushion(options = {})
           config = { decoder: nil }
           config.update(options) if options.is_a?(Hash)
 
-          class_eval <<-EOV
-            include ::ActiveRecord::Acts::Cushion::InstanceMethods
+          class_eval <<-EOF
+          include ActiveRecord::Acts::Cushion::InstanceMethods
+          EOF
 
-            def cushion_model_decoder
-              #{config[:decoder] || VelvetCushion.configuration.default_decoder}
-            end
-
-          EOV
-        end
-      end
-
-      # Methods available to "acts_as_cushion" models.
-      module InstanceMethods
-
-        def stuff_with(source, options = {})
-          config = { decoder: cushion_model_decoder }
-          config.update(options) if options.is_a?(Hash)
-
-          decoder = cushion_get_decoder_instance(config)
-
-          hash = decoder.decode(source)
-
-          stuff_from_hash hash
+          @cushion_model_decoder = config[:decoder] || VelvetCushion.configuration.default_decoder
         end
 
-        def stuff_from_hash(hash)
-
-        end
-
-        private
-
-        def cushion_get_decoder_instance(config)
-          config[:decoder] ||= cushion_model_decoder
-
-          decoder_classname = ActiveSupport::Inflector.camelize(config[:decoder])
-          Object.const_get("VelvetCushion::Decoders::#{decoder_classname}").new
+        def cushion_model_decoder
+          @cushion_model_decoder
         end
 
       end
+
     end
   end
 end
